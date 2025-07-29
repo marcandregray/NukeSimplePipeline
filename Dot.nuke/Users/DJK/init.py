922a945f6511f8712bdca0ef9b828b353211b2d4
Explodius <davidkossvfx@gmail.com>
2025-07-29 13:13:05 -0700

    Add files via upload

---
+++ Dot.nuke/Users/DJK/init.py
@@ -0,0 +1,10 @@
+# ----------------------------------
+# DJK init.py
+# Version: 1.0.0
+# Last Updated: 2025/07/24
+# ----------------------------------
+
+nuke.pluginAddPath('gizmos')
+nuke.pluginAddPath('gizmos/DK_Gizmos')
+nuke.pluginAddPath('python')
+#nuke.pluginAddPath('stamps')
