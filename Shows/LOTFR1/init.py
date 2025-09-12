a6d7127da6390aa4210ffb61a1692e95a37b3b9e
davidkossvfx <davidkossvfx@gmail.com>
2025-09-11 21:02:43 -0700

    Delete Shows directory

--- Shows/LOTFR1/init.py
+++
@@ -1,8 +0,0 @@
-# ----------------------------------
-# LOTFR1 init.py
-# Version: 1.0.0
-# Last Updated: 2025/07/24
-# ----------------------------------
-
-nuke.pluginAddPath('gizmos')
-nuke.pluginAddPath('python')
