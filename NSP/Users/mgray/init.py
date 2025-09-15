e007bce5c766d18c1a46b0c8c683198215592963
davidkossvfx <davidkossvfx@gmail.com>
2025-09-14 19:02:46 -0700

    Delete NSP directory

--- NSP/Users/mgray/init.py
+++
@@ -1,9 +0,0 @@
-# ----------------------------------
-# MAG init.py
-# Version: 1.0.0
-# Last Updated: 2025/07/24
-# ----------------------------------
-
-nuke.pluginAddPath('gizmos')
-nuke.pluginAddPath('python')
-nuke.pluginAddPath('ConnectDot')
