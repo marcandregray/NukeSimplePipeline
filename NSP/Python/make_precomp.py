eac9f38a60a456cd78e74e0f123e2aa8025745cb
davidkossvfx <davidkossvfx@gmail.com>
2025-10-16 12:52:09 -0700

    Add files via upload

---
+++ NSP/Python/make_precomp.py
@@ -0,0 +1,58 @@
+# make_precomp_v007
+
+import nuke
+
+def make_precomp():
+
+	#Create variables
+	show = nuke.tcl ("lrange [split [file rootname [file tail [value root.name]]] '_'] 0 end-3")
+	seq = nuke.tcl ("lrange [split [file rootname [file tail [value root.name]]] '_'] 1 end-2")
+	shot = nuke.tcl ("lrange [split [file rootname [file tail [value root.name]]] '_'] 2 end-1")
+	version = nuke.tcl ("lrange [split [file rootname [file tail [value root.name]]] '_'] end end")
+	padding = "_%04d"
+
+	def get_precomp_name():
+
+	    # Create the popup menu
+	    menu = nuke.Panel("Create Precomp")
+	    precompName = menu.addSingleLineInput("Precomp Name", "")
+	    extension = menu.addSingleLineInput("Extension", "exr")
+
+	    # Show the popup menu and get the user's input
+	    if menu.show():
+	        return menu.value("Precomp Name"), menu.value("Extension")
+	    else:
+	        return None, None
+
+	# Get the precomp name and extension from the user
+	precompName, extension = get_precomp_name()
+
+	# Create padding exception for mov
+	if extension == "mov":
+
+		padding = ""
+
+	# Check if the user entered a value
+	if precompName:
+		
+		# Create the procedural file Path and Name
+		procedural_file_path = f"{seq}/{shot}/Nuke/Renders/{precompName}/{version}/{show}_{seq}_{shot}_{precompName}_{version}{padding}.{extension}"
+
+		#Create write node
+		writeNode = nuke.createNode('NSP_Precomp.nk')
+		writeNode.knob('name').setValue("NSP_Precomp_" + precompName)
+		writeNode.knob('file').setValue(procedural_file_path)
+		writeNode.knob('proxy').setValue(procedural_file_path)
+		writeNode.knob('channels').setValue("rgba")
+		writeNode.knob('create_directories').setValue(1)
+		writeNode.knob('precomp_name').setValue(precompName)
+		writeNode.knob('extension').setValue(extension)
+
+		if extension == "exr":
+
+			writeNode.knob('dw_compression_level').setValue(100)
+			writeNode.knob('label').setValue("[value compression]")
+
+	else:
+		# User canceled the input, do nothing
+		pass
---
+++ NSP/init.py
@@ -0,0 +1,60 @@
+# ----------------------------------
+# init.py
+# Version: 1.2.1
+# Last Updated: 2025/10/08
+# ------------------------------------------------
+
+import nuke
+import os
+
+# ---------DEFINE CUSTOM FOLDER STRUCTURE----------
+
+nuke.pluginAddPath('Python')
+nuke.pluginAddPath('Icons')
+nuke.pluginAddPath('Gizmos')
+
+##====================================
+##============== README ==============
+##====================================
+
+# Below there are 3 variables that must be set in order for NSP to work correctly.
+# "projects_dir" must be set to a path that exists in your system, ideally where your projects are saved.
+# "user" and "show" must be set to a folder name that exists inside the "Users" and "Shows" folders respectively inside your .nuke/NSP folder.
+# If you don't want to use them, these variables can be set to "None" (don't include quotes around None).
+# After setting these variables, save this file and restart Nuke for it to work.
+# If there is a settings conflict, user settings will overwrite show settings.
+# For example: If you set the default resolution to HD in your user settings, but the show is set to 4K default, nuke will use HD as default.
+
+##====================================
+##====== Set Projects Directory ======
+##====================================
+
+# Set the "projects_dir" variable to the common path your projects share, like, "Common/Path/To/My/Projects"
+
+projects_dir = None
+
+##====================================
+##============ Set User ==============
+##====================================
+
+# Change the "user" variable to one that exists.
+
+user = None
+
+if user is not None:
+
+	userPath = os.path.join( "Users" , user )
+	nuke.pluginAddPath( userPath )
+
+##====================================
+##============ Set Show =============
+##====================================
+
+# Change the "show" variable to one that exists.
+
+show = None
+
+if show is not None:
+
+	showPath = os.path.join( "Shows" , show )
+	nuke.pluginAddPath( showPath )
